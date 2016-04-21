a6ae26687a77bcbc93894028a62b82c21e2e9b34
Jordan F Blocher <jblocher@unr.edu>
2016-04-21 14:00:28 -0700

    2 projects

---
+++ tron/Assets/Scripts/camera_switch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_switch : MonoBehaviour {
+
+	public Camera first;
+	public Camera third;
+	public UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (first.gameObject.activeInHierarchy) {
+				player.cam = third;
+				first.gameObject.SetActive (false);
+				third.gameObject.SetActive (true);
+			} else {
+				player.cam = first;
+				first.gameObject.SetActive (true);
+				third.gameObject.SetActive (false);
+			}
+		}
+	}
+}
