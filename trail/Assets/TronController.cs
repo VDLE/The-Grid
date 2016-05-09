bd195f5c33b7cfc101c74372d2f7c4efa4525801
Thomas Rushton <trushton134@gmail.com>
2016-05-09 14:15:02 -0700

    added trails

---
+++ trail/Assets/TronController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class TronController : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObj;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            AxisManager.pressed = 1;
+        }
+        else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            AxisManager.pressed = 0;
+        }
+    }
+}
