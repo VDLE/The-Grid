e8a952b03caef0ec3e63402d75b5f6b1607ca949
Jordan F Blocher <jblocher@unr.edu>
2016-05-09 14:02:44 -0700

    Exploding?

--- tron/Assets/AxisManager.cs
+++ tron/Assets/AxisManager.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class AxisManager2
+public static class AxisManager
 {
     public static int pressed;
+    public static float turn;
 }
