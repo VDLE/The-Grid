acad25474b71df48c04c60c23bf1b1772604f421
Thomas Rushton <trushton134@gmail.com>
2016-05-09 15:55:26 -0700

    potential explosions and light trails added

---
+++ tron/Assets/Light_Trail.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light_Trail : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject bike;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject trailObj = Instantiate(prefab, transform.position-transform.forward*5, bike.transform.rotation) as GameObject;
+        trailObj.AddComponent<Light_Timer>();
+    }
+}
